3ee87148a120cd6a2616873e1c5992a218fba1e0
C1210887 <c121088@e.koeki-u.ac.jp>
2023-07-17 21:34:54 +0900

    admin add connection button

--- host/admind/admin.ws.js
+++ host/admind/admin.ws.js
@@ -1,9 +1,37 @@
-function init() {
+function intro() {
   var coment = document.getElementById("innerHTMLtxt"),
-      AUDIO = document.getElementById("__音楽__");
-  //追加部分(WebSocketの部分)
-  var ws = new WebSocket('ws://localhost:8888/');
-  //var ws = new WebSocket("wss://www.koeki-prj.org/hayaoshi");
+      AUDIO = document.getElementById("__音楽__"),
+      ws;
+
+      function initConn(){    //接続確認
+        try{
+            ws = new WebSocket("ws://localhost:8888/")
+            //var ws = new WebSocket("wss://www.koeki-prj.org/hayaoshi");
+            ws.onopen = function() {};		// Nothing special
+            ws.onerror = function(err) {
+            alert('WebSocket failure: ' + err);
+            };
+            //var ws = new WebSocket('wss://www.koeki-prj.org/hayaoshi');
+            ws.onopen = function (ev) {
+                alert("接続完了");
+                coment.Context = "接続完了";
+                conn.disabled = true;
+            };
+            ws.onclose = function(ev){
+                coment.Context = "接続が行われていません。接続ボタンを押してもう一度お試しください";
+                conn.disabled = false;
+            };
+        }catch (err){
+            alert("Socket Creation Error" + err);
+        }
+    };
+    function con(ev){   //接続ボタンを押した時に接続確認
+            initConn();
+    }
+    var conn = document.getElementById("conn");
+    conn.addEventListener("mousedown" ,con,false);
+    initConn(); //最初に接続確認
+  
   function Decode(value) {
     //JSON ⇒ オブジェクト
     //エンコードされたデータを元の形式へ戻す。（デコード）
@@ -31,4 +59,4 @@ function init() {
   });
 }
 
-window.onload = init;
+document.addEventListener("DOMContentLoaded", intro, false);
