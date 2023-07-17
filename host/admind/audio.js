2c6b0b358a46501ffedca5eace149db1ec470461
C1210887 <c121088@e.koeki-u.ac.jp>
2023-07-17 23:27:36 +0900

    change stop button and play button

--- host/admind/audio.js
+++ host/admind/audio.js
@@ -1,6 +1,6 @@
 function playMusic() {
     // 音楽再生のためのオーディオ要素を取得
-    var audio = document.getElementById("music");
+    var audio = document.getElementById("__音楽__");
 
     // 音楽を再生
     audio.play();
@@ -9,7 +9,7 @@ function playMusic() {
   // ボタンがクリックされたときに音楽を停止する関数
   function stopMusic() {
     // 音楽再生のためのオーディオ要素を取得
-    var audio = document.getElementById("music");
+    var audio = document.getElementById("__音楽__");
 
     // 音楽を停止
     audio.pause();
