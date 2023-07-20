869fb72aad62474ce2f1b8a7724f6f0af8d0c510
C1210887 <c121088@e.koeki-u.ac.jp>
2023-07-20 22:38:57 +0900

    main encapsulation

--- host/maind/main.js
+++ host/maind/main.js
@@ -1,19 +1,25 @@
-function updateName() {
+(() => {
+    var nm = document.getElementById("nameBtn"),
+    tm = document.getElementById("teamBtn");
+    nm.addEventListener("mousedown" ,updateName,false);
+    function updateName() {
     var nameInput = document.getElementById("name");
     var name = nameInput.value;
 
     var nameText = document.getElementById("nameText");
     nameText.innerHTML = name;
     nameInput.disabled = true
-}
-
-function selectTeam() {
-    var teamSelect = document.getElementById("team");
-    var selectedTeam = teamSelect.options[teamSelect.selectedIndex].text;
-
-    var teamText = document.getElementById("teamText");
-    teamText.innerHTML = selectedTeam;
-
+    }
     
-}
+    tm.addEventListener("mousedown" ,updateTeam,false);
+    function done() {
+        var teamSelect = document.getElementById("team");
+        var selectedTeam = teamSelect.options[teamSelect.selectedIndex].text;
+
+        var teamText = document.getElementById("teamText");
+        teamText.innerHTML = selectedTeam;
+
+        
+    }
+})();
 
