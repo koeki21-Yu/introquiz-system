e217a566aed3d9f35c707265d3f268b3565c6a9e
C1210887 <c121088@e.koeki-u.ac.jp>
2023-08-14 02:52:26 +0900

    Downgrade

--- host/maind/main.js
+++ host/maind/main.js
@@ -1,30 +1,31 @@
-(() => {
-    function updatebtn(){
-        var nm = document.getElementById("nameBtn"),
-        tm = document.getElementById("teamBtn");
-        nm.addEventListener("mousedown",updateName,false);
-        tm.addEventListener("mousedown" ,updateTeam,false);
-        function updateName() {
-            var nameInput = document.getElementById("name");
-            var name = nameInput.value;
-        
-            var nameText = document.getElementById("nameText");
-            nameText.innerHTML = name;
-            nameInput.disabled = true
-        }
-        
-        function updateTeam() {
-            var teamSelect = document.getElementById("team");
-            var selectedTeam = teamSelect.options[teamSelect.selectedIndex].text;
+// (() => {
+function updatebtn(){
+    var nm = document.getElementById("nameBtn"),
+    tm = document.getElementById("teamBtn");
+    nm.addEventListener("mousedown",updateName,false);
+    tm.addEventListener("mousedown" ,updateTeam,false);
+    function updateName() {
+        var nameInput = document.getElementById("name");
+        var name = nameInput.value;
     
-            var teamText = document.getElementById("teamText");
-            teamText.innerHTML = selectedTeam;
-        }
-        
+        var nameText = document.getElementById("nameText");
+        nameText.innerHTML = name;
+        nameInput.disabled = true
     }
-    document.addEventListener("DOMContentLoaded", ()=>{
-        nm.addEventListener("mousedown" ,()=>{
-            updatebtn();
-        },false);
-    },false);
-})();
+    
+    function updateTeam() {
+        var teamSelect = document.getElementById("team");
+        var selectedTeam = teamSelect.options[teamSelect.selectedIndex].text;
+
+        var teamText = document.getElementById("teamText");
+        teamText.innerHTML = selectedTeam;
+    }
+    
+}
+document.addEventListener("DOMContentLoaded",updatebtn,false);
+//     document.addEventListener("DOMContentLoaded", ()=>{
+//         nm.addEventListener("mousedown" ,()=>{
+//             updatebtn();
+//         },false);
+//     },false);
+// })();
